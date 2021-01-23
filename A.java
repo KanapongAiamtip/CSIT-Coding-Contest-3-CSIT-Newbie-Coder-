ac3572420358c5267681ba0b08b5bf006c56b01d
KanapongAiamtip <75485361+KanapongAiamtip@users.noreply.github.com>
2021-01-24 03:07:06 +0700

    Update A.java

--- A.java
+++ A.java
@@ -1,1 +1,14 @@
-
+import java.util.Scanner;
+public class A 
+{
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        for (int i = x; i >= 1; i--) 
+        {
+            System.out.print(" " + i);
+        }
+        System.out.print(" GO ");
+    }
+}
